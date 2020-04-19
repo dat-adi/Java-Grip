80e2fec6be521b7e1fa1d28f9fbd1de090f0cb64
Dat Adithya <naruita201@gmail.com>
2020-04-19 14:53:33 +0530

    Added in an SLL

---
+++ online_programs/eleLinked.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class eleLinked{
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private static Node head = null;
+    private static Node tail = null;
+
+    private static Scanner sc = new Scanner(System.in);
+
+    private static void create_link(int size){
+        for(int i = 0; i < size; i++){
+            Node temp = new Node(sc.nextInt());
+            if(head == null)head = tail = temp;
+            else{
+                tail.next = temp;
+                tail = temp;
+            }
+        }
+    }
+
+    private static void show_link(){
+        tail = head;
+        while(tail != null){
+            System.out.println(tail.data);
+            tail = tail.next;
+        }
+    }
+
+    public static void main(String[] args){
+        int size = sc.nextInt();
+        create_link(size);
+        show_link();
+        sc.close();
+    }
+}
