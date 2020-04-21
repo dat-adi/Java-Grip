827e4c4a4954c4d52ae2358c717b6236d75b3b57
Dat Adithya <naruita201@gmail.com>
2020-04-21 12:04:44 +0530

    Static Initializer added

---
+++ online_programs/static_intializer.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class static_intializer {
+    static Scanner sc = new Scanner(System.in);
+    static int B = sc.nextInt();
+    static int H = sc.nextInt();
+    static boolean flag;
+    static{    
+        if(B >= 0 && H >= 0)flag = true;
+        else{
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    } 
+    public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}
+}
