896cff9ed16b44013484633e6ccd36758cf1584a
dat-adi <dat.adithya@gmail.com>
2020-07-14 08:42:35 +0530

    streamTest.java for using stream(), filter(), and mapToInt().

---
+++ library_studies/Arrays_lib/streamTest.java
@@ -0,0 +1,25 @@
+package library_studies.Arrays_lib;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+
+public class streamTest {
+	private static void stream_out(){
+		String[] str = {"A", "Fine", "Day"};
+		Stream<String> stream = Arrays.stream(str);
+		stream.forEach(System.out::print);
+	}
+	
+	private static void convert(){
+		List<String> list = Arrays.asList("3", "6", "8", "14", "15");
+		list.stream().mapToInt(Integer::parseInt)
+					.filter(s -> s%5 == 0)
+					.forEach(System.out::println);
+	}
+	
+	public static void main(String[] args) {
+		stream_out();
+		convert();
+	}
+}
