452cf7063b573aadf27469f28420828afddd827b
dat-adi <dat.adithya@gmail.com>
2020-07-17 12:24:12 +0530

    Testing out generic methods.

---
+++ library_studies/generic/generic_test.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class generic_test{
+
+    public static < E > void printArray( E[] arr){
+        for( E element : arr){
+            System.out.println(element);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        Integer[] intArr = {12, 32, 52, 56, 23};
+        Character[] charArr = {'Y', 'o', 'i', 't', 's', 'm', 'e'};
+
+        System.out.print("Integer array : ");
+        printArray(intArr);
+
+        System.out.println("Char array : ");
+        printArray(charArr);
+    }
+}
+
