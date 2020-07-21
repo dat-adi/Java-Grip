1a9fad66e84e99df6728bafc5617f2fedd3c5971
dat-adi <dat.adithya@gmail.com>
2020-07-21 21:49:17 +0530

    Added in trailing_zeros, not sure why unacceptable

---
+++ online_programs/trailing_zeros.java
@@ -0,0 +1,21 @@
+public class trailing_zero {
+  public static int zeros(int n) {
+    long fact = 1;
+    for(int i = 1; i <= n; i++){
+      fact = fact*i;
+      System.out.println("fact : " + fact + ", i :" + i);
+    }
+    System.out.println("Factorial : " + fact);
+    int count = 0;
+    while(fact > 0){
+      if(fact%10 == 0)
+        count++;
+      fact /= 10;
+    }
+    return count;
+  }
+
+  public static void main(String[] args){
+    System.out.println(zeros(0));
+  }
+}
