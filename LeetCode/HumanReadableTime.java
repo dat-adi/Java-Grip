c9636b3f18cca2e996f9291168cf17de26963ef3
dat-adi <dat.adithya@gmail.com>
2021-01-01 12:15:33 +0530

    Deleted the class, and committed the file.

--- LeetCode/HumanReadableTime.java
+++ LeetCode/HumanReadableTime.java
@@ -1,11 +1,12 @@
+package LeetCode;
+
 import java.util.Scanner;
 
 public class HumanReadableTime{
     private static String makeReadable(int seconds){
         int rem_seconds = seconds%60;
         int minutes = seconds/60;
-        String time = minutes + ":" + rem_seconds;
-        return time;
+        return minutes + ":" + rem_seconds;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
