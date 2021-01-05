9c92d6d95291905ad3719d27b67f9401fb478c34
dat-adi <dat.adithya@gmail.com>
2021-01-05 08:09:22 +0530

    Added a few examples

---
+++ Generics/LeastFrequent.java
@@ -0,0 +1,44 @@
+package Generics;
+
+import java.util.Arrays;
+
+public class LeastFrequent {
+	public static <E> E lesfreq(E[] arr, int n){
+		
+		Arrays.sort(arr);
+		
+		int min_count = n+1;
+		E res = null;
+		int curr_count = 1;
+		
+		for (int i = 1; i < n; i++) {
+			if (arr[i] == arr[i - 1])
+				curr_count++;
+			else {
+				if (curr_count < min_count) {
+					min_count = curr_count;
+					res = arr[i - 1];
+				}
+				
+				curr_count = 1;
+			}
+		}
+		
+		// If last element is least frequent
+		if (curr_count < min_count)
+		{
+			min_count = curr_count;
+			res = arr[n - 1];
+		}
+		
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		Integer[] intArr = {12, 32, 52, 56, 23, 12, 12, 42, 23, 32, 46, 56};
+		Character[] charArr = {'h', 'e', 'l', 'l', 'o', 'v', 'i', 't', 'a', 'p'};
+		
+		System.out.println(lesfreq(intArr, intArr.length));
+		System.out.println(lesfreq(charArr, charArr.length));
+	}
+}
